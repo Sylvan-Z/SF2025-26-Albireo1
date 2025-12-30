6495a5da10e5b087cca4bedfa71b0f7187c12739
Sylvan-Z <Sylvanz20@gmail.com>
2025-12-30 01:21:36 -0700

    Created gradient optimization, and generalized for n dimensions. Floating point errors are becoming noticable

---
+++ Airframe/Simulations/ML Testing/POC/src/optimizer.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import keras
+
+from numpy import arange, array, clip
+from itertools import product
+
+filepath = input("Filepath: ")
+
+model = keras.models.load_model(filepath+"/model.keras")
+
+searchStep = 0.01
+
+allFeatures = list(map(list,product(list(arange(0, 1+searchStep, searchStep)), repeat=2)))
+print(allFeatures)
+allDrags = list(map(lambda x: x[0], model.predict(array(allFeatures))))
+
+bestFeatures=[]
+bestdrag=float("inf")
+
+for i in range(len(allFeatures)):
+    if allDrags[i]<bestdrag:
+        bestdrag=allDrags[i]
+        bestFeatures=allFeatures[i]
+
+print("Done broad search")
+print(format("Predicted %s as the best with drag %f" % (str(bestFeatures), bestdrag)))
+open(filepath+"/log.txt","w+").write(format("Predicted %s as the best with drag %f\n Actual drag: " % (str(bestFeatures), bestdrag)))
+print("Starting gradient descent refinement")
+
+deltaF=0.0001
+deltaT=0.0001
+derivitives=[float("inf")]
+iterations=0
+while sum(map(lambda x: abs(x), derivitives))/len(derivitives)>0.0001:
+    derivitives=[]
+    for i in range(len(bestFeatures)):
+        dFeatures=bestFeatures
+        dFeatures[i]+=deltaF
+        dDrag=model.predict(array([dFeatures]))[0][0]
+        derivitives.append((dDrag-bestdrag)/deltaF)
+    for i in range(len(bestFeatures)):
+        bestFeatures[i]-=derivitives[i]*deltaT
+        bestFeatures[i]=clip(bestFeatures[i],0,1)
+    bestdrag=model.predict(array([bestFeatures]))[0][0]
+    iterations+=1
+    print("Iteration %d: Predicted %s as the best with drag %f, average gradient %f" % (iterations, str(bestFeatures), bestdrag, sum(derivitives)/len(derivitives)))
+
+print("Done refinement")
+print(format("Predicted %s as the best with drag %f" % (str(bestFeatures), bestdrag)))
